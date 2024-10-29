5d735c2eca04d440e543ffb158b275e2fadd2417
remi <remi.jarre@epitech.eu>
2024-10-29 17:03:25 +0100

    feat: change the landing page to something more like a presentation of the extension

--- finna/src/App.js
+++ finna/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +7,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Téléchargez notre extension web révolutionnaire: Finna !</h1>
-        <p>
-          Retrouvez les sources de ce que vous lisez! Simple, rapide et efficace.
-        </p>
+        <h1>Finna : Votre Extension Révolutionnaire</h1>
+      </header>
+
+      {/* Section par slide */}
+      <section className="slide">
+        <h2>Qu'est-ce que Finna ?</h2>
+        <p>Finna est une extension web conçue pour vous aider à retrouver les sources des informations que vous lisez en ligne.</p>
+      </section>
+
+      <section className="slide">
+        <h2>Pourquoi utiliser Finna ?</h2>
+        <p>Elle est simple, rapide et vous garantit une transparence totale des sources.</p>
+      </section>
+
+      <section className="slide">
+        <h2>Disponible sur les principaux navigateurs</h2>
+        <p>Compatible avec Chrome, Firefox, Edge et, peut-être, Safari.</p>
+      </section>
+
+      <section className="slide download-slide">
+        <h2>Prêt à l'essayer ?</h2>
         <a
           className="App-download"
           href="./extension-finna-test.html"
           download
         >
-          <button className="download-button">Télécharger l'extension</button>
+          <button className="download-button">Télécharger Finna</button>
         </a>
-        <p className="small-text">
-          Disponible sur Chrome, Firefox, Edge et Safari (je crois).
-        </p>
-      </header>
+      </section>
     </div>
   );
 }
