d918b67a2dfc4358c976906bb734d98700527b55
remi <remi.jarre@epitech.eu>
2024-12-16 15:20:05 +0100

    feat: add a documentation section within the landing page to more clarity, the documentation needs to be upgraded tho

--- finna/src/App.js
+++ finna/src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Features from "./components/Features";
 import DownloadSection from "./components/DownloadSection";
 import Footer from "./components/Footer";
+
+import Documentation from "./components/Documentation";
+
 import "./App.css";
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
     <div className="App">
       <Header />
       <Features />
+
+      <Documentation />
+      
       <DownloadSection />
       <Footer />
     </div>
