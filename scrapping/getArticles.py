f2f574474d845d8f1a81fcf5d29776c15eaaad64
Augustin Camus <augustin.camus@epitech.eu>
2024-12-29 17:58:17 +0900

    feat: the article are now inserted into an online DB

--- scrapping/getArticles.py
+++ scrapping/getArticles.py
@@ -1,9 +1,28 @@
 import requests
 import json
-import html2text
+import psycopg2
+from datetime import datetime
+
+# Establish a connection to the database
+conn = psycopg2.connect('placeholder')
+
+
+# Create a table to store the articles if it doesn't exist
+cur = conn.cursor()
+cur.execute('''
+CREATE TABLE IF NOT EXISTS article_tab (
+    title VARCHAR(255),
+    article TEXT,
+    date VARCHAR(255),
+    url VARCHAR(255),
+    provider VARCHAR(255)
+)
+''')
+conn.commit()
+
 
 # Discord webhook URL
-webhook_url = "https://discord.com/api/webhooks/1305055687755436032/iEB9LeV03w24kgVHZsb5iwM6OfprhTiKmNTWAeGiiyST-59sgtdBV3CZRHBx7p68_V--"
+webhook_url = "placeholder"
 
 def send_discord_message(message):
     """Send an error message to a Discord channel using a webhook."""
@@ -31,7 +50,7 @@ def fetch_data(url):
 url = "https://assets.msn.com/service/news/feed/pages/channelfeed?apikey=0QfOX3Vn51YCzitbLaRkTTBadtWpgTN8NZLW0C1SEM&cm=fr-fr&it=web&memory=8&ocid=social-peregrine&scn=ANON&timeOut=2000&user=m-3D0848EB4F8D67FD322A5DF54EA566BB"
 
 urls = []
-for _ in range(5):
+for _ in range(1):
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for HTTP errors
@@ -67,7 +86,31 @@ for url in urls:
         continue
 
     try:
-        data = response.json()
-        print(data['title'] + ", " + data['provider']['name'])
+        data = fetch_data(article_url)
+        title = data['title']
+        article = data.get('article', '')
+        date_str = data.get('publishedDateTime')
+        provider = data['provider']['name']
+        providerUrl = data['sourceHref']
+        print(article_url)
+
+        # Convert date string to date object
+        if date_str:
+            date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').date()
+        else:
+            date_obj = None
+
+        # Insert the data into the database
+        cur.execute('''
+        INSERT INTO article_tab (title, article, date, url, provider)
+        VALUES (%s, %s, %s, %s, %s)
+        ''', (title, article, date_obj, providerUrl, provider))
+
+        print(f"Inserted article: {title}, {provider}")
+
     except (json.JSONDecodeError, KeyError) as e:
         send_discord_message(f"Error parsing article JSON or missing data: {e}")
+
+conn.commit()
+cur.close()
+conn.close()
