b0b239916bf72f0640cbb46591a2a1b02b5a2516
Joris Blondeau <joris.blondeau@epitech.eu>
2025-07-04 14:32:16 +0200

    fix: new database template

--- src/database/dbInit.sql
+++ src/database/dbInit.sql
@@ -1,8 +1,190 @@
-CREATE TABLE IF NOT EXISTS Article_Tab (
-    ID SERIAL PRIMARY KEY,
-    Title VARCHAR(2083) NOT NULL,
-    Article TEXT NOT NULL,
-    Date DATE DEFAULT CURRENT_DATE,
-    URL VARCHAR(2083) NOT NULL,
-    Provider VARCHAR(255) NOT NULL
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.9
+-- Dumped by pg_dump version 16.9 (Ubuntu 16.9-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: unaccent; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
+
+
+--
+-- Name: article_tsv_trigger(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.article_tsv_trigger() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW.tsv := to_tsvector(
+    'french',
+    unaccent(coalesce(NEW.title, ''))
+    || ' '
+    || unaccent(coalesce(NEW.article, ''))
+  );
+  RETURN NEW;
+END;
+$$;
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: article_tab; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.article_tab (
+    id integer NOT NULL,
+    title character varying(2083) NOT NULL,
+    article text NOT NULL,
+    date date DEFAULT CURRENT_DATE,
+    url character varying(2083) NOT NULL,
+    provider character varying(255) NOT NULL,
+    tsv tsvector
 );
+
+
+--
+-- Name: article_tab_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.article_tab_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: article_tab_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.article_tab_id_seq OWNED BY public.article_tab.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(255),
+    provider_name text,
+    provider_token character varying(255),
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT users_provider_name_check CHECK ((provider_name = ANY (ARRAY['google'::text, 'facebook'::text, 'github'::text])))
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: article_tab id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.article_tab ALTER COLUMN id SET DEFAULT nextval('public.article_tab_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: article_tab article_tab_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.article_tab
+    ADD CONSTRAINT article_tab_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users unique_provider; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_provider UNIQUE (provider_name, provider_token);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: idx_article_tsv_gin; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX idx_article_tsv_gin ON public.article_tab USING gin (tsv);
+
+
+--
+-- Name: article_tab tsvectorupdate; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE ON public.article_tab FOR EACH ROW EXECUTE FUNCTION public.article_tsv_trigger();
+
+
+--
+-- PostgreSQL database dump complete
+--
+
